b3e05abddaba07cb49f8b241ab5997f6be125955
RamyaRami <107798301+RamyaRami@users.noreply.github.com>
2022-07-07 23:06:01 +0530

    Delete mostfrequent.py

--- mostfrequent.py
+++
@@ -1,9 +0,0 @@
-str=input("Enter a string")
-count={}
-for x in str:
-    if x in count.keys():
-        count[x]+=1
-    else:
-        count[x]=1
-for x in count.keys():
-    print(x,"=",count[x])
